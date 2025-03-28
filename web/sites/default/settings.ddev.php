a02ad90137e8182238974e7d642fc76be5a531f3
Mykhailo Hurei <mykhailo.gurei@gmail.com>
2025-03-28 15:25:33 +0200

    Add Drupal skeleton

---
+++ web/sites/default/settings.ddev.php
@@ -0,0 +1,47 @@
+<?php
+
+$host = "db";
+$port = 3306;
+$driver = "mysql";
+
+$databases['default']['default']['database'] = "db";
+$databases['default']['default']['username'] = "db";
+$databases['default']['default']['password'] = "db";
+$databases['default']['default']['host'] = $host;
+$databases['default']['default']['port'] = $port;
+$databases['default']['default']['driver'] = $driver;
+
+$settings['hash_salt'] = 'f13c2c29674ea18dd268c47e00d49abc33baa298d53df81286f797309c79b22c';
+
+// This will prevent Drupal from setting read-only permissions on sites/default.
+$settings['skip_permissions_hardening'] = TRUE;
+
+// This will ensure the site can only be accessed through the intended host
+// names. Additional host patterns can be added for custom configurations.
+$settings['trusted_host_patterns'] = ['.*'];
+
+// Set $settings['config_sync_directory'] if not set in settings.php.
+if (empty($settings['config_sync_directory'])) {
+  $settings['config_sync_directory'] = 'sites/default/files/sync';
+}
+
+// Override drupal/symfony_mailer default config to use Mailpit.
+$config['symfony_mailer.settings']['default_transport'] = 'sendmail';
+$config['symfony_mailer.mailer_transport.sendmail']['plugin'] = 'smtp';
+$config['symfony_mailer.mailer_transport.sendmail']['configuration']['user'] = '';
+$config['symfony_mailer.mailer_transport.sendmail']['configuration']['pass'] = '';
+$config['symfony_mailer.mailer_transport.sendmail']['configuration']['host'] = 'localhost';
+$config['symfony_mailer.mailer_transport.sendmail']['configuration']['port'] = '1025';
+
+// Enable verbose logging for errors.
+// https://www.drupal.org/forum/support/post-installation/2018-07-18/enable-drupal-8-backend-errorlogdebugging-mode
+$config['system.logging']['error_level'] = 'verbose';
+$settings['cache']['bins']['discovery_migration'] = 'cache.backend.memory';
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$settings['cache']['bins']['page'] = 'cache.backend.null';
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
+$settings['extension_discovery_scan_tests'] = FALSE;
+
+$settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml';
